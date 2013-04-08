cb8c49f5c7665232081161c67a5b008e33407ddc
Nu Va Suparati <dev@nuvasuparati.info>
2013-04-08 20:27:00 +0300

    added english and romanian just to make it work

---
+++ lib/public_body_categories_en.rb
@@ -0,0 +1,8 @@
+PublicBodyCategories.add(:en, [
+    "Ministere",
+        [ "Ministere", "Ministere", "ministere ale României" ],
+    "Ambasade",
+        [ "Ambasade", "Ambasade", "ambasade ale altor țări în România"],
+    "Primării",
+        [ "Primării", "Primării", "primării române"]
+])
